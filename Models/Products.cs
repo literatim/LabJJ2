0dbbc43bc87bb6ce2f9d7d465126beda5da69e3c
Ben Jen <benjendrzejak@quickenloans.com>
2019-01-15 09:17:02 -0500

    First commit

---
+++ Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabJJ2.Models
+{
+    public class Products
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
